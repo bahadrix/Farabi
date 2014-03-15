a5c374f9ada036c888f9c07e6670822ce6c6da51
Umutcan Güney <umutcang@gmail.com>
2014-03-15 19:44:32 +0200

    FFT changes. Added licence blocks for jtransforms.

--- src/main/java/me/farabi/audio/dsp/fft/FFT.java
+++ src/main/java/me/farabi/audio/dsp/fft/FFT.java
@@ -3,7 +3,7 @@ package me.farabi.audio.dsp.fft;
 /**
  * Created by umutcanguney on 15/03/14.
  * Calculates the FFT of a given byte array.
- * https://github.com/JorenSix/TarsosDSP
+ * inspired from https://github.com/JorenSix/TarsosDSP
  */
 
 public class FFT {
@@ -37,8 +37,31 @@ public class FFT {
         this.fft.complexForward(data);
     }
 
+    public float[] transformAndGetMagnitudes(final float[] data) {
+        forwardTransform(data);
+        float[] magnitudes = calculatePowers(data);
+        return magnitudes;
+    }
+
     public double binToHz(final int binIndex, final float sampleRate) {
         return binIndex * sampleRate / (double) this.fftSize;
+    }
+
+    public float calculatePower(float real, float im, final int index) {
+        return (float) Math.sqrt(real*real + im*im);
+    }
+
+    public float[] calculatePowers(final float[] data) {
+        float[] magnitudes = new float[data.length / 2];
+        calculatePowers(data, magnitudes);
+        return magnitudes;
+    }
+
+    public void calculatePowers(final float[] data, final float[] magnitudes) {
+        int l = magnitudes.length;
+        for (int i = 0; i < l; i++) {
+            magnitudes[i] = calculatePower(data[2*i], data[2*i + 1], i);
+        }
     }
 
 }
